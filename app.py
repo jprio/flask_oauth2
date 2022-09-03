6eb8df74c00187d82974ecfdfad448a95f6f999e
jprio <jeanpierre.rio@gmail.com>
2022-09-03 08:53:17 +0200

    init

---
+++ app.py
@@ -0,0 +1,47 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask, render_template, redirect, url_for
+from flask_dance.contrib.google import make_google_blueprint, google
+import logging
+
+load_dotenv()
+app = Flask(__name__)
+
+print(os.getenv('secret_key'))
+client_id = os.getenv('GOOGLE_CLIENT_ID')
+client_secret = os.getenv('GOOGLE_CLIENT_SECRET')
+app.config['SECRET_KEY'] = os.getenv('secret_key')
+app.config['SESSION_TYPE'] = 'filesystem'
+
+
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
+
+blueprint = make_google_blueprint(
+    client_id=client_id,
+    client_secret=client_secret,
+    reprompt_consent=True,
+    scope=["profile", "email"]
+)
+app.register_blueprint(blueprint, url_prefix="/login")
+
+
+@app.route("/")
+def index():
+    google_data = None
+    user_info_endpoint = '/oauth2/v2/userinfo'
+    if google.authorized:
+        google_data = google.get(user_info_endpoint).json()
+
+    return render_template('index.j2',
+                           google_data=google_data,
+                           fetch_url=google.base_url + user_info_endpoint)
+
+
+@app.route('/login')
+def login():
+    return redirect(url_for('google.login'))
+
+
+if __name__ == "__main__":
+    app.run()
